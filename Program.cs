273d535329b354f969cfbaa57d8e7f52e40203ee
KaveLizard <lifmi123@gmail.com>
2023-12-10 21:40:39 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,35 +1,27 @@
-using TestApplication.EventDbContext;
-using TestApplication.Repositories;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Builder;
+using BlazorApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<EventContext>(options => options.UseSqlServer(ConfigurationExtensions.GetConnectionString(builder.Configuration, "DefaultConnection")));
-builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
-
-app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.MapControllers();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
-
---
+++ Services/EventService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApplication.EventDbContext;
+using TestApplication.Models;
+
+namespace TestApplication.Services
+{
+    public interface IEventService
+    {
+        Task<List<Event>> GetEvents();
+    }
+
+    public class EventService : IEventService
+    {
+        private readonly EventContext _dbContext;
+
+        public EventService(EventContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Event>> GetEvents()
+        {
+            return _dbContext.Events.ToList();
+        }
+        public async Task<List<Event>> GetEventById(int id)
+        {
+            return _dbContext.Events.Take(id).ToList();
+        }
+        
+
+        Task<List<Event>> IEventService.GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
