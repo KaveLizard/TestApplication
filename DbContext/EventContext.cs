3235382b65adc0d86d51f7b30c459fbd747134c3
KaveLizard <lifmi123@gmail.com>
2023-12-10 23:07:54 +0300

    Revert "commit"
    This reverts commit 881ab4f35994a6bb8809e2b61dd6479e672355af.

---
+++ DbContext/EventContext.cs
@@ -0,0 +1,12 @@
+﻿using TestApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestApplication.EventDbContext
+{
+    public class EventContext : DbContext
+    {
+       public EventContext(DbContextOptions<EventContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
